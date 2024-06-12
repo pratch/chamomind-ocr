ad417b3786ddb2f2c6cf685451c28215a1d934be
Pratch P <hakatachi@gmail.com>
2024-06-13 01:12:14 +0700

    Commented out debug print

--- document_processing.py
+++ document_processing.py
@@ -8,7 +8,7 @@ def identify_doc_type(img, ocr_results):
     has_juris_record_title = any(
         item[1] == 'หนังสือรับรอง' for item in ocr_results)
     has_juris = any('นิติบุคคล' in item[1] for item in ocr_results)
-    print(has_juris_record_title, has_juris)
+    # print(has_juris_record_title, has_juris)
     is_juris_record = has_juris_record_title and has_juris
 
     if is_juris_record:
@@ -33,7 +33,7 @@ def extract_fields(img, ocr_results, doc_type):
         target_field_corner = np.asarray((row.x1, row.y1))
         
         # find closest ocr corner
-        # TODO: try find ocr boxes with highest IoU with csv boxes instead?
+        # TODO: try finding ocr box with highest IoU with csv box instead?
         dist_2 = np.sum((ocr_result_corners - target_field_corner)**2, axis=1)
         closest_idx = np.argmin(dist_2)
         
